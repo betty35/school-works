3c919c8ff336ed090307bf9b315b7f002ba55bde
betty35 <betty352008@yeah.net>
2016-03-25 22:12:00 +0800

    Some little things
    x16 xor, modular division, and a function I can't even remember what's
    for.... Something written to help writing school work of Cryptograph

---
+++ 16进制xor/16进制xor/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _16进制xor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.moddiv = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(56, 19);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(208, 21);
+            this.tb1.TabIndex = 0;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(56, 46);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(208, 21);
+            this.tb2.TabIndex = 1;
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(56, 76);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(208, 21);
+            this.sum.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "16进制xor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "加数1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "加数2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "结果";
+            // 
+            // moddiv
+            // 
+            this.moddiv.Location = new System.Drawing.Point(281, 61);
+            this.moddiv.Name = "moddiv";
+            this.moddiv.Size = new System.Drawing.Size(151, 30);
+            this.moddiv.TabIndex = 7;
+            this.moddiv.Text = "模态除法(1:数 2:模)";
+            this.moddiv.UseVisualStyleBackColor = true;
+            this.moddiv.Click += new System.EventHandler(this.moddiv_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "order(1:生成数 2:模)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.moddiv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Name = "Form1";
+            this.Text = "16进制xor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button moddiv;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 16进制xor/16进制xor/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _16进制xor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = tb1.Text;
+            string s2 = tb2.Text;
+            string s3; string s4;
+            sum.Text = "";
+            if (s1.Length > 8)
+            {
+                for (int i = 0; i < s1.Length / 8; i++)
+                {
+                    s3 = s1.Substring(i * 8, 8);
+                    s4 = s2.Substring(i * 8, 8);
+                    long dec1 = Convert.ToInt64(s3, 16);
+                    long dec2 = Convert.ToInt64(s4, 16);
+                    long d1 = dec1 ^ dec2;
+                    sum.Text += d1.ToString("x8");
+                }
+            }
+            
+            
+
+        }
+
+        private void moddiv_Click(object sender, EventArgs e)
+        {
+            int t = Int16.Parse(tb1.Text);
+            int b = Int16.Parse(tb2.Text);
+            long c=0;
+            for (int a = 1; a < b; a++)
+            {
+                c = t * a % b;
+                if (c == 1)
+                {sum.Text = a.ToString(); break; }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int g = Int16.Parse(tb1.Text);
+            int b = Int16.Parse(tb2.Text);
+            int[] c = new int[b+1];
+            int count=0;
+            double d = 0;
+            for (int a = 1; a < b; a++)
+            {
+                Boolean same = false;
+                d = Math.Pow(g, a)%b;
+                for (int i = 1; i < b+1; i++)
+                {
+                    if (d == c[i])
+                        same = true;
+                }
+                if (same!=true)
+                {
+                    c[a] = (int)d;
+                    count++;
+                }
+
+            }
+            sum.Text = count.ToString();
+        }
+    }
+}
